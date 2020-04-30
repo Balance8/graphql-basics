e3d240b54a06505bd195e49a860b7938300bfdb6
Balance8 <mslocum819@gmail.com>
2020-04-29 18:41:20 -0700

    post pubsub

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -103,7 +103,7 @@ const Mutation = {
 
     return post;
   },
-  createPost(parent, args, { db }, info) {
+  createPost(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
 
     if (!userExists) {
@@ -116,6 +116,10 @@ const Mutation = {
     };
 
     db.posts.push(post);
+
+    if (args.data.published) {
+      pubsub.publish(`post`, { post });
+    }
 
     return post;
   },
