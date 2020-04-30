e0af64efe389dc4007fcac8fc2dcb263b9fde5a5
Balance8 <mslocum819@gmail.com>
2020-04-29 17:18:48 -0700

    pubsub comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 //db
 import db from './db';
@@ -6,21 +6,26 @@ import db from './db';
 // Resolvers
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
 import User from './resolvers/User';
 import Post from './resolvers/Post';
 import Comment from './resolvers/Comment';
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment,
   },
   context: {
     db,
+    pubsub,
   },
 });
 
---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,31 @@
+const Subscription = {
+  count: {
+    subscribe(parent, args, { pubsub }, info) {
+      let count = 0;
+
+      setInterval(() => {
+        count++;
+        pubsub.publish('count', {
+          count,
+        });
+      }, 1000);
+
+      return pubsub.asyncIterator('count');
+    },
+  },
+  comment: {
+    subscribe(parent, { postId }, { db, pubsub }, info) {
+      const post = db.posts.find(
+        (post) => post.id === postId && post.published
+      );
+
+      if (!post) {
+        throw new Error('Unable to find post');
+      }
+
+      return pubsub.asyncIterator(`comment ${postId}`); // comment 44 example
+    },
+  },
+};
+
+export { Subscription as default };
