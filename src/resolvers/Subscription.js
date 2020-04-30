4ccfe0a2b0f28dc355c39a1cd1eb6719e0c857c2
Balance8 <mslocum819@gmail.com>
2020-04-29 18:42:28 -0700

    remove count

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,18 +1,4 @@
 const Subscription = {
-  count: {
-    subscribe(parent, args, { pubsub }, info) {
-      let count = 0;
-
-      setInterval(() => {
-        count++;
-        pubsub.publish('count', {
-          count,
-        });
-      }, 1000);
-
-      return pubsub.asyncIterator('count');
-    },
-  },
   comment: {
     subscribe(parent, { postId }, { db, pubsub }, info) {
       const post = db.posts.find(
@@ -28,7 +14,7 @@ const Subscription = {
   },
   post: {
     subscribe(parent, args, { db, pubsub }, info) {
-      return pubsub.asyncIterator(`post`); 
+      return pubsub.asyncIterator(`post`);
     },
   },
 };
